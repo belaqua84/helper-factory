d811aede7f434d3762259968742668cf81165b76
Tracy Hamer <tracy.hamer@gmail.com>
2015-03-18 21:24:58 -0700

    initial commit

---
+++ helperFactory.js
@@ -0,0 +1,93 @@
+(function(){ //encapsulating the factory keeps its contents out of the global namespace.
+
+    'use strict';
+
+	var helperFactory = function(){
+        return {
+            debounce: function(func, wait, immediate){              // Returns a function that as long as it continues to be invoked, will not
+                var timeout;                                        // be triggered. The function will be called after it stops being invoked for
+                return function() {                                 // N milliseconds. If `immediate` is passed, trigger the function on the
+                    var context = this, args = arguments;           // leading edge, instead of the trailing.
+                    var later = function() {
+                        timeout = null;
+                        if (!immediate) func.apply(context, args);
+                    };
+                    var callNow = immediate && !timeout;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(later, wait);
+                    if (callNow) func.apply(context, args);
+                };
+            },
+            // forEach method to iterate over nodelist (presumably) returned by querySelectorAll
+            forEachNode: function(nodeList, callback, model){
+                for (var i = 0; i < nodeList.length; i++) {
+                    callback.call(model, i, nodeList[i]); // passes back the stuff we need
+                }
+            },
+            hasClass: function(elem, selector){
+                var className = " " + selector + " ";
+                var cleanClass = /[\n\t\r]/g;
+                if ((" " + elem.className + " ").replace(cleanClass, " ").indexOf(className) > -1) {
+                    return true;
+                }
+
+                return false;
+            },
+            addClass: function(elem, addClass){
+                var classArray = (elem.className).split(' ');
+                var classAlreadyExists = false;
+                classArray.forEach(function(currentClass){
+                    if(currentClass === addClass){
+                        classAlreadyExists = true;
+                    }
+                });
+                if(classAlreadyExists === false){
+                    elem.className += ' ' + addClass;
+                }
+            },
+            removeClass: function(elem, removeClass){
+                var classArray = (elem.className).split(' ');
+                var removedClassList = '';
+                classArray.forEach(function(currentClass){
+                    if(currentClass !== removeClass){
+                        removedClassList += ' ' + currentClass;
+                    }
+                });
+                elem.className = removedClassList.replace(/^\s+|\s+$/g,''); //trim trailing and leading white space
+            },
+            whichBrowser: function(){
+                var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+                // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+                var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+                var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+                // At least Safari 3+: "[object HTMLElementConstructor]"
+                var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+                var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+                var browser;
+
+                switch(true){
+                    case isChrome:
+                        browser = 'chrome';
+                        break;
+                    case isSafari:
+                        browser = 'safari';
+                        break;
+                    case isOpera:
+                        browser = 'opera';
+                        break;
+                    case isFirefox:
+                        browser = 'firefox';
+                        break;
+                    case isIE:
+                        browser = 'ie';
+                        break;
+                }
+
+                return browser;
+            }
+        };
+	};
+
+	angular.module('yourApp').factory('helperFactory', helperFactory);
+}());
